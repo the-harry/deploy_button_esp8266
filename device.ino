e9728ebc34fe793393b8ab77938d17802fef1839
Matheus Acosta <matheusthebr@gmail.com>
2020-08-23 17:40:20 -0300

    Updated code to use dip switch format of env.

--- device.ino
+++ device.ino
@@ -4,57 +4,69 @@
 
 #define BUTTON D0
 
-#define L_RED D1
-#define L_YELLOW D2
-#define L_GREEN D8
+#define L_GREEN D7
+#define L_RED D8
 
-#define SSID "WIFI"
-#define PASSWORD "PASS"
+#define SSID "A1"
+#define PASSWORD "SENAH"
 #define SERVER "http://192.168.0.17:6666/api/v1/deploy"
 #define API_KEY "{~c[3^n3u7R-j/By"
+
+void unconnected_warning() {
+  digitalWrite(L_GREEN, HIGH);
+  digitalWrite(L_RED, HIGH);
+  delay(500);
+  digitalWrite(L_GREEN, LOW);
+  digitalWrite(L_RED, LOW);
+}
+
+void success_warning() {
+  digitalWrite(L_GREEN, HIGH);
+  delay(5000);
+  digitalWrite(L_GREEN, LOW);
+}
+
+void error_warning() {
+  digitalWrite(L_RED, HIGH);
+  delay(5000);
+  digitalWrite(L_RED, LOW);
+}
+
+int deploy_request() {
+  HTTPClient http;
+  http.begin(SERVER);
+
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("API_KEY", API_KEY);
+  int res = http.POST("{\"environment\":\"0000\"}");
+  http.end();
+
+  return res;
+}
 
 void setup() {
   pinMode(BUTTON, INPUT);
   pinMode(L_RED, OUTPUT);
-  pinMode(L_YELLOW, OUTPUT);
   pinMode(L_GREEN, OUTPUT);
 
   WiFi.begin(SSID, PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(L_YELLOW, HIGH);
-    delay(500);
+    unconnected_warning();
   }
-
-  digitalWrite(L_YELLOW, LOW);
 }
 
 void loop() {
   if (digitalRead(BUTTON) == LOW) {
     if (WiFi.status() == WL_CONNECTED) {
-      HTTPClient http;
-      http.begin(SERVER);
-
-      http.addHeader("Content-Type", "application/json");
-      http.addHeader("API_KEY", API_KEY);
-      int response = http.POST("{\"environment\":\"staging\"}");
-
-      if (response == 201) {
-        digitalWrite(L_GREEN, HIGH);
-        delay(5000);
-        digitalWrite(L_GREEN, LOW);
+      if (deploy_request() == 201) {
+        success_warning();
       } else {
-        digitalWrite(L_RED, HIGH);
-        delay(5000);
-        digitalWrite(L_RED, LOW);
+        error_warning();
       }
-
-      http.end();
     }
     else {
-      digitalWrite(L_YELLOW, HIGH);
-      delay(2000);
-      digitalWrite(L_YELLOW, LOW);
+      unconnected_warning();
     }
   }
 
